83f38e5d35cdbb731ca2fe590cdf5da129a810fc
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-04-27 09:20:00 +0000

    1.2

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require('express');
 const webServer = express();
 const port = 30120;
 console.log("Lotteria realizzata da")
-console.log("Alessandro Pallaro e Luca Gregorio Buscema")
+console.log("Alessandro Pallaro e Luca Buscema")
 const router = require('./router.js');
 
 webServer.set('view engine', 'ejs');
@@ -11,6 +11,13 @@ webServer.use('/', router);
 
 webServer.use('/event', express.static('evento'));
 
-webServer.listen(port, function(){
-  console.log(`Il server ascolta alla porta: ${port}!`)
+
+webServer.listen(port, function(){ //funzione per output in console
+const P = ['/','-', '|'];
+let x = 0;
+const loader = setInterval(() => {
+  process.stdout.write(`\r${P[x++]}`);
+  x %= P.length;
+}, 150);
+// console.log('Programma in esecuzione alla porta: '${port})
 });
