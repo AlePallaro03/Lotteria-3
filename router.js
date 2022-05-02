2d427ae2c12cebbab5cd24d275fa5060b221acc4
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-05-02 19:05:38 +0000

    1.2.4

--- router.js
+++ router.js
@@ -2,7 +2,7 @@ const express= require('express');
 const router = express.Router();
 
 router.get('/event',function(req, res){
-  res.redirect('/event/login.html') //se viene eseguita in URL la dicitura /event verremo reinderizzati alla nostra pagina
+  res.redirect('/event/index.html') //se viene eseguita in URL la dicitura /event verremo reinderizzati alla nostra pagina
 });
 
 
