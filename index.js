3312f03ad2a3716922ebff523bb912eae819a3b5
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-05-02 19:20:34 +0000

    1.2.5

--- index.js
+++ index.js
@@ -1,13 +1,21 @@
 const express = require('express'); //importiamo modulo esterno (express)
 const webServer = express();
 const port = 30120; //numero della porta
-const nomi = ['Alessandro Pallaro', 'Luca Buscema'] //constante coi nomi dei Developer
-console.log("Lotteria realizzata da") //output
-console.log() //spazio in console
-console.log(nomi) //output con la variabile creata
-console.log() //spazio in console
+console.log("Lotteria realizzata da: ") //output
+console.log(`
+ █████╗ ██╗     ███████╗    ███████╗    ██╗     ██╗   ██╗██╗  ██╗
+██╔══██╗██║     ██╔════╝    ██╔════╝    ██║     ██║   ██║██║ ██╔╝
+███████║██║     █████╗      █████╗      ██║     ██║   ██║█████╔╝ 
+██╔══██║██║     ██╔══╝      ██╔══╝      ██║     ██║   ██║██╔═██╗ 
+██║  ██║███████╗███████╗    ███████╗    ███████╗╚██████╔╝██║  ██╗
+╚═╝  ╚═╝╚══════╝╚══════╝    ╚══════╝    ╚══════╝ ╚═════╝ ╚═╝  ╚═╝
+  `)
+
+
 console.log("Inserire /event nell'URL per aprire la lotteria") //output
-console.log() //spazio in console
+
+
+
 const router = require('./router.js');
 
 webServer.use('/', router); 
