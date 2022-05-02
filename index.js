6e5a24cc44c28a91a7e8d3550486d96962fd8817
PALLARO <102584752+AlePallaro03@users.noreply.github.com>
2022-05-02 17:50:18 +0000

    1.2.3

--- index.js
+++ index.js
@@ -1,23 +1,23 @@
-const express = require('express');
+const express = require('express'); //importiamo modulo esterno (express)
 const webServer = express();
-const port = 30120;
-console.log("Lotteria realizzata da")
-console.log()
-console.log("Alessandro Pallaro e Luca Buscema")
-console.log()
-console.log("Inserire /event nell'URL per aprire la lotteria")
-console.log()
+const port = 30120; //numero della porta
+const nomi = ['Alessandro Pallaro', 'Luca Buscema'] //constante coi nomi dei Developer
+console.log("Lotteria realizzata da") //output
+console.log() //spazio in console
+console.log(nomi) //output con la variabile creata
+console.log() //spazio in console
+console.log("Inserire /event nell'URL per aprire la lotteria") //output
+console.log() //spazio in console
 const router = require('./router.js');
 
-webServer.use('/', router);
-webServer.use('/event', express.static('evento'));
-
+webServer.use('/', router); 
+webServer.use('/event', express.static('evento')); //MIDDLEWARE
 webServer.listen(port, function(){ //funzione per output in console
-const P = ['/','-', '|']; 
+const P = ['/','-', '|']; //vettore con simboli
 let x = 0;
 const loader = setInterval(() => {
   process.stdout.write(`\r${P[x++]}`);
   x %= P.length;
 }, 150);
-console.log(`Programma in esecuzione alla porta: ${port}`)
+console.log(`Programma in esecuzione alla porta: ${port}`) //output in console
 });
--- router.js
+++ router.js
@@ -2,8 +2,8 @@ const express= require('express');
 const router = express.Router();
 
 router.get('/event',function(req, res){
-  res.redirect('/event/login.html')
+  res.redirect('/event/login.html') //se viene eseguita in URL la dicitura /event verremo reinderizzati alla nostra pagina
 });
 
 
-module.exports=router;
+module.exports=router; //esportiamo il modulo express
